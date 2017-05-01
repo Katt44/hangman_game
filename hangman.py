23c242b42b49fd0caea9e639478a7c0a8b443bb4
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-30 20:18:42 -0700

    added pseudo code and set up empty functions

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+
+
+
+'''
+Pseudo Code:
+	| Start |
+	| secret word |
+	| show drawing |
+	| ask user for a letter | ->  | player already  guessed this letter|
+	| if letter is in word | -> | if letter is not in word| -> | limited guesses|
+	| player guessed all letters| -> |player ran out of guesses|
+	| ask player to play again | -> | start again |
+	|end|
+
+
+
+
+'''
+
+
+
+HANGMAN_PICS = [
+'' ''
+'' ''
+'' ''
+'' ''
+'' ''
+'' ''
+'' ''
+]
+
+
+# Functions
+
+def get_random_word():
+	pass
+def display_board():
+	pass
+def get_guess():
+	pass
+def play_again():
+	pass
+
+# Calls
